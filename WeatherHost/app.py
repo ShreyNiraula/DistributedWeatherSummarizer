5e195ba269e24dbfc1aee19d1e630c9a0e988889
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2024-11-13 18:41:59 -0600

    set up tailwind

--- WeatherHost/app.py
+++ WeatherHost/app.py
@@ -15,4 +15,4 @@ app.add_url_rule('/subscribe', 'subscribe', subscribe, methods=['POST'])
 app.add_url_rule('/trigger_alert', 'trigger_alert', trigger_alert, methods=['POST'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.getenv("VM1").split(":")[-1])
+    app.run(host='0.0.0.0', port=os.getenv("VM1").split(":")[-1],debug=True)
